b69f15732ef27b03727d49bfc07b8c43941a1174
shrverma <shrverma@expedia.com>
2019-08-26 18:11:25 +0530

    first commit

---
+++ src/main/java/com/snakeladder/game/snakeladder/Board.java
@@ -0,0 +1,5 @@
+package com.snakeladder.game.snakeladder;
+
+public class Board {
+
+}
---
+++ src/main/java/com/snakeladder/game/snakeladder/SnakeladderApplication.java
@@ -0,0 +1,13 @@
+package com.snakeladder.game.snakeladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeladderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeladderApplication.class, args);
+	}
+
+}
